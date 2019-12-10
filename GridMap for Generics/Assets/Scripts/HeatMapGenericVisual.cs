34a98a23f47940d4b8cfa97f260ad6e109d97f68
Christian Simmons <csimmonscs@gmail.com>
2019-12-10 07:56:14 -0800

    Uploading GenericsGridMap to Github

---
+++ GridMap for Generics/Assets/Scripts/HeatMapGenericVisual.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using System;
+
+public class HeatMapGenericVisual : MonoBehaviour
+{
+    private Grid<HeatMapGridObject> grid;
+    private Mesh mesh;
+    private bool updateMesh;
+
+    private void Awake()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    public void SetGrid(Grid<HeatMapGridObject> grid)
+    {
+        this.grid = grid;
+        UpdateHeatMapVisual();
+
+        grid.OnGridValueChanged += Grid_OnGridValueChanged;
+    }
+
+    private void Grid_OnGridValueChanged(object sender, Grid<HeatMapGridObject>.OnGridValueChangedEventArgs e)
+    {
+        //Debug.Log("Grin");
+        //UpdateHeatMapVisual();
+        updateMesh = true;
+    }
+
+    private void LateUpdate()
+    {
+        if (updateMesh)
+        {
+            updateMesh = false;
+            UpdateHeatMapVisual();
+        }
+    }
+
+    private void UpdateHeatMapVisual()//cycles every grid position and adds quad
+    {
+        MeshUtils.CreateEmptyMeshArrays(grid.GetWidth() * grid.GetHeight(), out Vector3[] vertices, out Vector2[] uv, out int[] triangles);
+        //uses CodeMonkey function to make mesh arrays
+
+        for (int x = 0; x < grid.GetWidth(); x++)
+        {
+            for(int y = 0; y < grid.GetHeight(); y++)
+            {
+                int index = x * grid.GetHeight() + y;
+                //Debug.Log(index);
+                Vector3 quadSize = new Vector3(1, 1) * grid.GetCellSize();
+
+                HeatMapGridObject gridObject = grid.GetGridObject(x, y);
+                float gridValueNormalized = gridObject.GetValueNormalized();
+                Vector2 gridValueUV = new Vector2(gridValueNormalized, 0f);
+                MeshUtils.AddToMeshArrays(vertices, uv, triangles, index, grid.GetWorldPosition(x,y) + quadSize * 0.5f, 0f, quadSize, gridValueUV, gridValueUV);
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+}
---
+++ GridMap for Generics/Assets/Scripts/Testing.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private HeatMapVisual heatMapVisual;
+    [SerializeField] private HeatMapBoolVisual heatMapBoolVisual;
+    [SerializeField] private HeatMapGenericVisual heatMapGenericVisual;
+    private Grid<HeatMapGridObject> grid;
+    private Grid<StringGridObject> stringGrid;
+
+    private void Start()
+    {
+        //grid = new Grid<HeatMapGridObject>(20, 10, 5f, Vector3.zero, (Grid<HeatMapGridObject> g, int x, int y) => new HeatMapGridObject(g, x, y));
+
+        stringGrid = new Grid<StringGridObject>(20, 10, 5f, Vector3.zero, (Grid<StringGridObject> g, int x, int y) => new StringGridObject(g, x, y));
+
+        //heatMapVisual.SetGrid(grid);
+        //heatMapBoolVisual.SetGrid(grid);
+        //heatMapGenericVisual.SetGrid(grid);
+    }
+
+    private void Update()
+    {
+        Vector3 position = UtilsClass.GetMouseWorldPosition();
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 position = UtilsClass.GetMouseWorldPosition();
+        //    //int value = grid.GetValue(position);
+        //    //grid.SetValue(position, value + 5);
+        //    //grid.AddValue(position, 100, 2, 20);
+        //    //grid.SetValue(position, true);
+        //    HeatMapGridObject heatMapGridObject = grid.GetGridObject(position);
+
+        //    if(heatMapGridObject != null)
+        //    {
+        //        heatMapGridObject.AddValue(5);
+        //    }
+        //}
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            stringGrid.GetGridObject(position).AddLetter("A");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            stringGrid.GetGridObject(position).AddLetter("B");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            stringGrid.GetGridObject(position).AddLetter("C");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            stringGrid.GetGridObject(position).AddNumber("1");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            stringGrid.GetGridObject(position).AddNumber("2");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            stringGrid.GetGridObject(position).AddNumber("3");
+        }
+    }
+}
+
+public class HeatMapGridObject{
+    private const int MIN = 0;
+    private const int MAX = 100;
+    private int value;
+    private Grid<HeatMapGridObject> grid;
+    private int x;
+    private int y;
+
+    public HeatMapGridObject( Grid<HeatMapGridObject> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+    }
+
+
+
+    public void AddValue(int addValue)
+    {
+        value += addValue;
+        Mathf.Clamp(addValue, MIN, MAX);
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public float GetValueNormalized()
+    {
+        return (float)value / MAX;
+    }
+
+    public override string ToString()
+    {
+        return value.ToString();
+    }
+}
+
+public  class StringGridObject
+{
+    private string letters;
+    private string numbers;
+
+    private Grid<StringGridObject> grid;
+    private int x;
+    private int y;
+
+    public StringGridObject(Grid<StringGridObject> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        letters = "";
+        numbers = "";
+     }
+
+    public void AddLetter(string letter)
+    {
+        letters += letter;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public void AddNumber(string number)
+    {
+        numbers += number;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public override string ToString()
+    {
+        return letters + "\n" + numbers;
+    }
+}
